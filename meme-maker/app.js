4e24cf490b989cc62fc6e3e546537edc1d2d1f14
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 11:49:43 +0900

    lecture 1.4 - Drawing house

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -4,24 +4,16 @@ const ctx = canvas.getContext("2d"); //brush
 canvas.width = 800;
 canvas.height = 800;
 
-// Lecture 1.2 - Path
-// // Path = Layer
-// // Path1
-// ctx.rect(50, 50, 100, 100); 
-// ctx.rect(200, 200, 100, 100); 
-// ctx.rect(300, 300, 100, 100); 
-// ctx.fill();
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
 
-// ctx.beginPath();
-// ctx.rect(400, 400, 100, 100); 
-// ctx.fillStyle = 'red';
-// ctx.fill();
+// code order matters
+ctx.lineWidth = 3; //making line thicker
+ctx.strokeRect(300, 300, 50, 100);
 
+ctx.fillRect(200, 200, 200, 30);
 
-// Lecture 1.3 - moveTo and lineTo
-ctx.moveTo(50, 50);
-ctx.lineTo(600, 50);
-ctx.lineTo(600, 600);
-ctx.lineTo(50, 600);
-ctx.lineTo(50, 90);
-ctx.stroke();
+ctx.moveTo(200, 200);
+ctx.lineTo(300, 100);
+ctx.lineTo(450, 200);
+ctx.fill()
