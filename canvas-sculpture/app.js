b7179a53e71006d4f38e76631560efa5b36351d2
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 17:26:12 +0900

    create canvas-sculpture version app

---
+++ canvas-sculpture/app.js
@@ -0,0 +1,102 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn")
+const colorOptions = Array.from(document.getElementsByClassName("color-option")); //Make HTML collection array to JS array
+const eraserBtn = document.getElementById("eraser-btn");
+
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d"); //brush
+
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 700;
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.lineWidth = 70;
+let isPainting = false;
+let isFilling = false;
+
+let rgb = 0;
+
+function onMove(e) {
+
+    if(isPainting) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+function onMouseDown() {
+    isPainting = true;
+}
+
+function onMouseUp() {
+    isPainting = false;
+}
+
+function onLineWidthChange(e) {
+    ctx.lineWidth = e.target.value;
+}
+
+function onColorChange(e) {
+    ctx.strokeStyle = e.target.value;
+    ctx.fillStyle = e.target.value; //for filling
+}
+
+function onColorClick(e) {
+    const colorValue = e.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue; //User color select -> changing color selector as well
+    rgb = 255;
+}
+
+function onModeClick() {
+    if(isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill"
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw"
+    }
+}
+
+function onCanvasClick() {
+    if(isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+    if(rgb > 255) {
+        rgb = 0;
+    }
+    rgb += 10;
+    ctx.strokeStyle = `rgba(${rgb}, ${rgb}, ${rgb}, 1)`
+    console.log(rgb);
+
+}
+
+function onDestroyClick() {
+    ctx.fillStyle="white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
