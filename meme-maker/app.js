691bd268cf5d0dbda6e75066c1742fda58dfbea9
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 14:19:31 +0900

    lecture 2.1 - mouse painting

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,27 +1,28 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); //brush
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 700;
+canvas.height = 700;
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-    "#ffcccc",
-    "#ffaf40",
-    "#fffa65",
-    "#4b4b4b",
-    "#cd84f1",
-    "#32ff7e"
-]
-
-function onClick(event) {
-    ctx.beginPath()
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    console.log(event);
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(e) {
+    if(isPainting) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(e.offsetX, e.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function onMouseDown() {
+    isPainting = true;
+}
+
+function onMouseUp() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
