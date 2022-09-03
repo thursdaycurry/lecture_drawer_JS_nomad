854c33dcb114b1af9fc0040ff59579eb7bbf61fe
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 16:21:02 +0900

    lecture 2.5  - Filling Mode

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option")); //Make HTML collection array to JS array
 
 const color = document.getElementById("color");
@@ -10,6 +11,7 @@ canvas.width = 700;
 canvas.height = 700;
 ctx.lineWidth = 70;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(e) {
     if(isPainting) {
@@ -35,7 +37,7 @@ function onLineWidthChange(e) {
 
 function onColorChange(e) {
     ctx.strokeStyle = e.target.value;
-    ctx.fillStyle = e.target.value;
+    ctx.fillStyle = e.target.value; //for filling
 }
 
 function onColorClick(e) {
@@ -45,10 +47,27 @@ function onColorClick(e) {
     color.value = colorValue; //User color select -> changing color selector as well
 }
 
+function onModeClick() {
+    if(isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill"
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw"
+    }
+}
+
+function onCanvasClick() {
+    if(isFilling) {
+        ctx.fillRect(0, 0, 700, 700);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
