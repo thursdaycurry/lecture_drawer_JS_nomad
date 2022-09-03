b3e27977b299437bc8964baf26f6dd2e34c08c36
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 15:35:41 +0900

    lecture 2.4 - paint color - add color selectors for user

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,11 +1,14 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option")); //Make HTML collection array to JS array
+
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); //brush
 
+
 canvas.width = 700;
 canvas.height = 700;
-ctx.lineWidth = lineWidth;
+ctx.lineWidth = 70;
 let isPainting = false;
 
 function onMove(e) {
@@ -35,8 +38,17 @@ function onColorChange(e) {
     ctx.fillStyle = e.target.value;
 }
 
+function onColorClick(e) {
+    const colorValue = e.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue; //User color select -> changing color selector as well
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
