b58dd186a4a4a48ae7ec498e75780ddf3b553fa0
Thursdaycurry <thursdaycurry@gmail.com>
2022-09-03 12:25:19 +0900

    lecture 2.0 - painting lines(cone creator - discotech versino)

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,11 +3,21 @@ const ctx = canvas.getContext("2d"); //brush
 
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
+
+const colors = [
+    "#ffcccc",
+    "#ffaf40",
+    "#fffa65",
+    "#4b4b4b",
+    "#cd84f1",
+    "#32ff7e"
+]
 
 function onClick(event) {
     ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
     console.log(event);
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
